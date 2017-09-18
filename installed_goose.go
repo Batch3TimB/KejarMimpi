6bf538ab617868c831f8af2487831333548b5c77
Isthofany Azkiya <fany@wonderlabs.io>
2017-09-18 22:12:03 +0700

    add godep

--- installed_goose.go
+++ installed_goose.go
@@ -1,6 +1,3 @@
-// use build constraints to work around http://code.google.com/p/go/issues/detail?id=4210
-// +build heroku
-
 // note: need at least one blank line after build constraint
 package main
 
